cd4bbb5e3220f944d8496a6a4112e3f785b4d2b7
dude <vmmgame84@gmail.com>
2023-12-16 19:47:41 -0600

    Dec 16 Final Changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { auth, db } from "../config/firebase";
 import Navbar from "./Navbar";
 import { Link } from "react-router-dom";
@@ -7,25 +7,33 @@ import { collection, query, where, limit, getDocs } from "firebase/firestore";
 const UserHomePage = () => {
   const [userData, setUserData] = useState(null);
   const userDocRef = collection(db, "users");
-  const queryUserData = async () => {
-    try {
-      const q = query(
-        userDocRef,
-        where("userId", "==", auth.currentUser.uid),
-        limit(1)
-      );
-      await getDocs(q);
-      console.log();
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  console.log(queryUserData());
+
+  useEffect(() => {
+    const queryUserData = async () => {
+      try {
+        const q = query(
+          userDocRef,
+          where("userId", "==", auth.currentUser.uid),
+          limit(1)
+        );
+        const queryResults = await getDocs(q);
+
+        queryResults.forEach((doc) => {
+          setUserData(doc.data());
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    queryUserData();
+  }, []);
+
   return (
     <div className="homeContainer">
       <Navbar />
       <div className="homePage">
-        <h1>Welcome Back {auth?.currentUser.displayName}</h1>
+        <h1>Welcome Back {userData ? userData.username : "No User Data"}</h1>
+        <Link to="/status">Start Tests</Link>
       </div>
     </div>
   );
